c9f4fd52b14f3a671a75d3a0bf0b826ee530cab7
Elijah Y <74162307+Lij175@users.noreply.github.com>
2023-03-29 09:48:23 -0700

    Add files via upload

---
+++ src/character.java
@@ -0,0 +1,17 @@
+public class character {
+    double size;
+    int[] orgin;
+    String name;
+ 
+    character(){
+        size = 1;
+    }
+
+    character(double size, int[] orgin, String name){
+        this.size = size;
+        this.orgin = orgin;
+        this.name = name;
+    }
+
+}
+
