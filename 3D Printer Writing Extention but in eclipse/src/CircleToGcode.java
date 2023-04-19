34faa71d21a5ff82d6b74074641bb0173256857b
Elijah Y <74162307+Lij175@users.noreply.github.com>
2023-04-19 09:31:46 -0700

    Add files via upload

---
+++ 3D Printer Writing Extention but in eclipse/src/CircleToGcode.java
@@ -0,0 +1,66 @@
+import java.util.ArrayList;
+
+public class CircleToGcode {
+        public static void main(String[] args) {
+        /*
+         given a starting point (a,b), and ending point (c,d), 
+         the center of the circle (h,k), and the radius (r), and 
+         clockwise or counter clockwise:
+
+         (r cos(i * 2pi/n) + h, r sin(i * 2pi/n) + k)
+         */
+
+        double pi = 3.1415926;
+        double start = -1 * pi /2;
+        double end = 3 * pi / 2;
+        boolean clockwise = false;
+        double rcos = 0.5;
+        double rsin = 0.5;
+        int n = 50;
+
+        ArrayList<double[]> pointsOnCurve = new ArrayList<double[]>();
+        if(clockwise){
+            for(double i = start - (2 * pi / n); i >= end; i -= (2 * pi / n)){
+                double x = rcos * Math.cos(i);
+                double y = rsin * Math.sin(i);
+
+                x = Math.round(x * 10000.0) / 10000.0;
+                y = Math.round(y * 10000.0) / 10000.0;
+
+                pointsOnCurve.add(new double[] {x, y});
+            }
+        } 
+        else{
+            for(double i = start + (2 * pi / n); i <= end; i += (2 * pi / n)){
+                double x = rcos * Math.cos(i);
+                double y = rsin * Math.sin(i);
+
+                x = Math.round(x * 10000.0) / 10000.0;
+                y = Math.round(y * 10000.0) / 10000.0;
+
+                pointsOnCurve.add(new double[] {x, y});
+            }
+        }
+
+        for(int i = 0; i < pointsOnCurve.size(); i ++){
+            double[] point = pointsOnCurve.get(i);
+            System.out.println("(" + point[0] + ", " + point[1] + ")");
+        }
+
+        System.out.println("\nGCODE:");
+
+        double[] prevPoint = new double[] {rcos * Math.cos(start), rsin * Math.sin(start)};
+        for(int i = 0; i < pointsOnCurve.size(); i ++){
+            double[] currPoint = pointsOnCurve.get(i);
+            double x = currPoint[0] - prevPoint[0];
+            double y = currPoint[1] - prevPoint[1];
+
+            x = Math.round(x * 10000.0) / 10000.0;
+            y = Math.round(y * 10000.0) / 10000.0;
+
+            System.out.println("G0 X" + x + " Y" + y + " E0.2");
+            prevPoint = currPoint;
+        }
+    }
+}
+
---
+++ 3D Printer Writing Extention but in eclipse/src/PrinterWritingExtention.java
@@ -0,0 +1,231 @@
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.Scanner;
+
+// IMPORTANT!! REMEMBER THIS:
+// 50 < X < 220
+// 50 < Y < 220
+public class PrinterWritingExtention {
+	// test
+
+	public static String user = "1082635";
+ 
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		// files and stuff
+		File skeleton = new File("C:\\Users\\" + user + "\\eclipse-workspace\\3D Printer Writing Extention but in eclipse\\gcodes\\WitingBaseCode.gcode");
+		File text_gcode = new File("C:\\Users\\" + user + "\\eclipse-workspace\\3D Printer Writing Extention but in eclipse\\gcodes\\text.gcode");
+		File text_txt = new File("C:\\Users\\" + user + "\\eclipse-workspace\\3D Printer Writing Extention but in eclipse\\gcodes\\text.txt");
+		// scanners
+		Scanner textScanner = new Scanner(text_txt);
+		Scanner skeletonScanner = new Scanner(skeleton);
+		PrintStream ps = new PrintStream(text_gcode);
+		Scanner userInput = new Scanner(System.in);
+
+		// text and specs
+		System.out.print("Text input(0) or read file(1): ");
+		boolean fileText = Integer.parseInt(userInput.nextLine()) == 1;
+
+		String text = "";
+		if (!fileText) {
+			System.out.print("What would you like to print: ");
+			text = userInput.nextLine();
+		}
+
+		System.out.print("font size?: ");
+		double fontSize = Double.parseDouble(userInput.nextLine());
+
+		if (!fileText) {
+			writeUserText(skeletonScanner, text, fontSize, ps);
+		} else {
+			Scanner textScannerForCheckingFontSize = new Scanner(text_txt);
+			
+			boolean isFontSizeGood = checkIfFontSizeGood(textScannerForCheckingFontSize, fontSize);
+			while(!isFontSizeGood) {
+				fontSize -= 0.1;
+				isFontSizeGood = checkIfFontSizeGood(textScannerForCheckingFontSize, fontSize);
+			}
+			
+			writeTextFile(skeletonScanner, fontSize, ps, textScanner);
+		}
+
+		userInput.close();
+		skeletonScanner.close();
+	}
+ 
+	public static boolean checkIfFontSizeGood(Scanner textScanner, double fontSize) {
+		while(textScanner.hasNext()) {
+			String newLine = textScanner.nextLine();
+			double lineLength = (newLine.length() * ((fontSize * 8) + 2));
+			if(lineLength > 220) {
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	// takes the text file and turns it into gcode
+	public static void writeTextFile(Scanner skeletonScanner, double fontSize, PrintStream ps, Scanner textScanner)
+			throws FileNotFoundException {
+		// loop to write gcode
+		while (skeletonScanner.hasNext()) {
+			String nextLine = skeletonScanner.nextLine();
+
+			// will copy stuff from base code unless stuff needs to go here
+			if (nextLine.equals("; Stuff Go Here")) {
+				// print stuff go here
+				ps.println(nextLine);
+
+				// initialization of stuff
+				int x = 50;
+				int y = 220;
+				while (textScanner.hasNext()) {
+					String newLine = textScanner.nextLine();
+					// loops through each char in the line
+					for (int i = 0; i < newLine.length(); i++) {
+						char character = newLine.charAt(i);
+
+						// space if space
+						if (character == ' ') {
+							x += (fontSize * 8) + 2;
+						}
+						// print new char if not a space
+						else {
+							character newChar = new character(fontSize, new int[] { x, y }, character + "");
+							addNewCharacter(newChar, ps);
+							x += (fontSize * 8) + 2;
+						}
+					}
+
+					// go to new line
+					y -= (fontSize * 13) + 2;
+					x = 50;
+				}
+
+			}
+
+			// copy base code
+			else {
+				ps.println(nextLine);
+			}
+		}
+	}
+
+	// takes the users text and turns it into gcode
+	public static void writeUserText(Scanner skeletonScanner, String text, double fontSize, PrintStream ps)
+			throws FileNotFoundException {
+		// loop to write gcode
+		while (skeletonScanner.hasNext()) {
+			String nextLine = skeletonScanner.nextLine();
+
+			// will copy stuff from base code unless stuff needs to go here
+			if (nextLine.equals("; Stuff Go Here")) {
+				// print stuff go here
+				ps.println(nextLine);
+
+				// initialization of stuff
+				int x = 50;
+				int y = 220;
+				String[] words = text.split(" ");
+
+				for (String word : words) {
+					// if word length goes past bound go to new line
+					if (x + (word.length() * (fontSize * 8 + 2)) > 220) {
+						y -= (fontSize * 13) + 2;
+						x = 50;
+
+						// if the word is still too long, check each letter before going to the next
+						// line
+						if ((word.length() * (fontSize * 8 + 2)) > 220) {
+							for (int i = 0; i < word.length(); i++) {
+								if (x + (fontSize * 8 + 2) > 220) {
+									y -= (fontSize * 13) + 2;
+									x = 50;
+								}
+								character newChar = new character(fontSize, new int[] { x, y }, word.charAt(i) + "");
+								addNewCharacter(newChar, ps);
+								x += (fontSize * 8) + 2;
+							}
+
+							// space
+							x += (fontSize * 8) + 2;
+							continue;
+						}
+					}
+
+					// print word
+					for (int i = 0; i < word.length(); i++) {
+						character newChar = new character(fontSize, new int[] { x, y }, word.charAt(i) + "");
+						addNewCharacter(newChar, ps);
+						x += (fontSize * 8) + 2;
+					}
+
+					// space
+					x += (fontSize * 8) + 2;
+				}
+
+			}
+
+			// copy base code
+			else {
+				ps.println(nextLine);
+			}
+		}
+	}
+
+	// gets and adds new character gcode
+	public static void addNewCharacter(character newChar, PrintStream ps) throws FileNotFoundException {
+		// one character
+		ps.println();
+		ps.println("G0 X" + newChar.orgin[0] + " Y" + newChar.orgin[1] + "; go to char orgin");
+		ps.println("G0 Z1 ; lower to 1");
+		ps.println("G91 ; relitive positioning");
+
+		ps.println();
+
+		// gets file
+		File gcode;
+		Scanner gcodeScanner;
+		try {
+			gcode = new File("C:\\Users\\" + user + "\\eclipse-workspace\\3D Printer Writing Extention but in eclipse\\gcodes\\characters\\"+ newChar.name + ".gcode");
+			gcodeScanner = new Scanner(gcode);
+		} catch (Exception FileNotFoundException) {
+			gcode = new File("C:\\Users\\" + user + "\\eclipse-workspace\\3D Printer Writing Extention but in eclipse\\gcodes\\characters\\box.gcode");
+			gcodeScanner = new Scanner(gcode);
+		}
+
+		while (gcodeScanner.hasNext()) {
+			String gcodeLine = gcodeScanner.nextLine();
+
+			// changes size if line is not a comment or moves up
+			if (!gcodeLine.contains("G1 Z") && !gcodeLine.contains(";")) {
+				String[] gcodeLineToArr = gcodeLine.split(" ");
+
+				// gets x and y and multiples by size
+				double x = Double.parseDouble(gcodeLineToArr[1].substring(1)) * newChar.size;
+				double y = Double.parseDouble(gcodeLineToArr[2].substring(1)) * newChar.size;
+				gcodeLineToArr[1] = "X" + x;
+				gcodeLineToArr[2] = "Y" + y;
+
+				// round
+				x = Math.round(x * 10000.0) / 10000.0;
+				y = Math.round(y * 10000.0) / 10000.0;
+
+				// re-make line
+				gcodeLine = "";
+				for (int i = 0; i < gcodeLineToArr.length; i++) {
+					gcodeLine += gcodeLineToArr[i] + " ";
+				}
+
+			}
+			ps.println(gcodeLine);
+		}
+
+		ps.println("G90 ; absolute positioning");
+		ps.println("G1 Z1 ; raise a bit");
+		gcodeScanner.close();
+	}
+
+}
