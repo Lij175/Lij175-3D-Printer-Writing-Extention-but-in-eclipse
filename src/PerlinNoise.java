29d92c45fd762ac97067abf028e2a24f12488cd5
Elijah Y <74162307+Lij175@users.noreply.github.com>
2023-05-26 10:10:03 -0700

    Add files via upload

--- src/PerlinNoise.java
+++ src/PerlinNoise.java
@@ -1,15 +1,17 @@
-
+import java.awt.Graphics;
 
 public class PerlinNoise {
 	
+	static double smoothness = 0.5;
+	static int size = 250;
+	static double[][] PerlinMap = new double [size][size];
+	static double[][][] Vectors = new double [size][size][2];
+	
 	public static void main(String[] args) {
-		double smoothness = 0.5;
-		int size = 10;
-		double[][] PerlinMap = new double [size][size];
-		double[][][] Vectors = new double [size][size][2];
-		
+		new MyFrame();
+		/**
 		for(int i = 0; i < size; i++){
-			for(int j = 0; j < size; j++) {
+			for(int j = 0; j < size; j++) {s
 				//(r cos(i * 2pi/n) + h, r sin(i * 2pi/n) + k)
 				int rand = (int)Math.random() * 50;
 				double x = Math.cos(rand * Math.PI / 25);
@@ -22,11 +24,11 @@ public class PerlinNoise {
 		}
 		
 		double[] input = new double[] {Math.random(),  Math.random()};
-		
-		// fill map with random 1, 0
+		**/
+		// fill map with random 1, 0 (value Noise)
 		for(int i = 0; i < size; i++){
 			for(int j = 0; j < size; j++) {
-				PerlinMap[i][j] = 1;
+				PerlinMap[i][j] = Math.random();
 			}
 		}
 		
